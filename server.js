f9e8b431345afa1d54b9fa9b8fddffdf5940ee3a
Jose Federico <federicj@gmatc.matc.edu>
2022-03-18 23:55:36 -0500

    Completed Activity 7

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post(
         check('name', 'Please enter your name')
             .not()
             .isEmpty(),
-        check('email', 'Please enter a valid emial').isEmail(),
+        check('email', 'Please enter a valid email').isEmail(),
         check(
             'password',
             'Please enter a password with 6 or more characters'
