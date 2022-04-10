269b8a9d5a67475c883e92b2dc27d7f1237e850c
Jose Federico <federicj@gmatc.matc.edu>
2022-04-10 11:39:31 -0500

    Updated GET

--- server.js
+++ server.js
@@ -198,7 +198,20 @@ app.post(
         }
     }
 );
-
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+ app.get('/api/posts', auth, async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ date: -1 });
+  
+      res.json(posts);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+  });
 
 // Connection listener
 const port = 5000;
