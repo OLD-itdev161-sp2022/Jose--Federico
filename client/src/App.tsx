55a8280f78a7ef0dc30add1bbc9a99c5c302ba6a
Jose Federico <federicj@gmatc.matc.edu>
2022-04-24 11:52:28 -0500

    Added Post Components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import PostList from './components/PostList/PostList';
+import Post from './components/Post/Post';
 
 class App extends React.Component {
 
     state = {
         posts: [],
-        data: null,
+        post: null,
         token: null,
         user: null
     }
@@ -59,7 +61,7 @@ class App extends React.Component {
                 }
             };
             axios
-                .get('/api/posts/', config)
+                .get('http://localhost:5000/api/posts', config)
                 .then(response => {
                     this.setState({
                         posts: response.data
@@ -91,8 +93,13 @@ class App extends React.Component {
         this.authenticateUser();
     }
 
+    viewPost = post => {
+        console.log(`view ${post.title}`);
+        this.setState({ post: post });
+    }
+
     render() {
-        let { user, posts } = this.state;
+        let { user, posts, post } = this.state;
         const authProps = {
             authenticateUser: this.authenticateUser
         };
@@ -114,32 +121,30 @@ class App extends React.Component {
                         </ul>
                     </header>
                     <main>
-                        <Route exact path="/">
-                            {user ? (
-                                <React.Fragment>
-                                    <div>Hello {user}!</div>
-                                    <div>
-                                        {posts.map(post => (
-                                            <div key={post._id}>
-                                                <h1>{post.title}</h1>
-                                                <p>{post.body}</p>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    Please Register or Login
-                                </React.Fragment>
-                            )}
-                        </Route>
                         <Switch>
+                            <Route exact path="/">
+                                {user ? (
+                                    <React.Fragment>
+                                        <div>Hello {user}!</div>
+                                        <PostList posts={posts} clickPost={this.viewPost} />
+                                    </React.Fragment>
+                                ) : (
+                                    <React.Fragment>Please Register or Login</React.Fragment>
+                                )}
+                            </Route>
+                            <Route path="/posts/:postId">
+                                <Post post={post} />
+                            </Route>
                             <Route
-                                exact path="/register"
-                                render={() => <Register {...authProps} />} />
+                                exact
+                                path="/register"
+                                render={() => <Register {...authProps} />} 
+                            />
                             <Route
-                                exact path="/login"
-                                render={() => <Login {...authProps} />} />
+                                exact
+                                path="/login"
+                                render={() => <Login {...authProps} />} 
+                                />
                         </Switch>
                     </main>
                 </div>
