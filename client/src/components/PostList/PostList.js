abcfcbf02fa7348a676a02a402bfdc5025928dab
Jose Federico <federicj@gmatc.matc.edu>
2022-04-24 12:03:54 -0500

    Added Create

--- client/src/components/PostList/PostList.js
+++ client/src/components/PostList/PostList.js
@@ -2,7 +2,7 @@ import PostListItem from './PostListItem';
 import React from 'react';
 
 const PostList = props => {
-    const { posts, clickPost } = props;
+    const { posts, clickPost, deletePost } = props;
     return posts.map(post => (
         <PostListItem
             key={post._id}
